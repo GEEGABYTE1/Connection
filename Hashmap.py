63b38581127d1122ef544c0a2824f9a3aa172e37
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-06-02 20:36:19 -0400

    Removed Separate Chaining

--- Hashmap.py
+++ Hashmap.py
@@ -1,9 +1,9 @@
-from double_ll import DoubleLinkedList
+
 
 class HashMap:
     def __init__(self, array_size):
         self.array_size = array_size 
-        self.array = [DoubleLinkedList() for i in range(self.array_size)]
+        self.array = [None for i in range(self.array_size)]
 
     def hash(self, key, collisions=0):
         key_bytes = str(key).encode()
@@ -16,103 +16,87 @@ class HashMap:
     def setter(self, key, value):
         array_index = self.compressor(self.hash(key))
         current_array_value = self.array[array_index]
-        current_ll_head_node = current_array_value.get_head_node()
 
-        if current_ll_head_node == None:
-            current_array_value.add_to_head([key, value])
+        if current_array_value == None:
+            self.array[array_index] = [key, value]
             return 
         
-        if current_ll_head_node.get_value()[0] == key:
-            current_array_value.add_to_tail([key, value])
+        elif current_array_value[0] == key:
+            self.array[array_index] = [key, value]
             return 
+
         
         number_collisions = 1
-        while current_ll_head_node.get_value()[0] != key:
-            new_hash_code = self.hash(key ,number_collisions)
+        while current_array_value[0] != key:
+            new_hash_code = self.hash(key, number_collisions)
             new_array_index = self.compressor(new_hash_code)
             new_array_value = self.array[new_array_index]
-            new_ll_head_node = new_array_value.get_head_node()
 
-            if new_ll_head_node == None:
-                current_array_value.add_to_head([key, value])
+            if new_array_value == None:
+                self.array[new_array_index] = [key, value]
                 return 
             
-            if new_ll_head_node.get_value()[0] == key:
-                new_array_value.add_to_tail([key, value])
+            elif new_array_value[0] == key:
+                self.array[new_array_index] = [key, value]
                 return 
             
             number_collisions += 1
             return 
-
-
+    
     def retrieve(self, key):
-        self.returns = []
         array_index = self.compressor(self.hash(key))
         current_array_value = self.array[array_index]
-        current_ll_head_node = current_array_value.get_head_node()
+        self.returns = [] 
 
-        if current_ll_head_node == None:
-            print("There is no server there! ")
-            return self.returns  
+        if current_array_value == None:
+            return None 
+        elif current_array_value[0] == key:
+            self.returns.append(current_array_value)
+            return 
         
-        if current_ll_head_node.get_value()[0] == key:
-            current_node = current_ll_head_node
-            while current_node:
-                self.returns.append(current_node)
-                current_node = current_node.get_link()
-            
         retrieve_collisions = 1
-        
-        while current_ll_head_node.get_value()[0] != key:
-            new_hash = self.hash(key, retrieve_collisions)
-            new_array_index = self.compressor(new_hash)
+        while current_array_value[0] != key:
+            new_hash_code = self.hash(key, retrieve_collisions)
+            new_array_index = self.compressor(new_hash_code)
             new_array_value = self.array[new_array_index]
-            new_ll_head_node = new_array_value.get_head_node()
 
-            if new_ll_head_node == None:
-                print("There is no server there! ")
-                return self.returns
+            if new_array_value == None:
+                return None 
             
-            if new_ll_head_node.get_value()[0] == key:
-                current_node = new_ll_head_node
-                while new_ll_head_node:
-                    self.returns.append(new_ll_head_node)
-                    current_node = current_node.get_link()
+            elif new_array_value[0] == key:
+                self.returns.append(new_array_value)
+                return 
             
             retrieve_collisions += 1
             return 
         
         return self.returns 
 
-    
     def delete(self, key):
         array_index = self.compressor(self.hash(key))
         current_array_value = self.array[array_index]
-        current_ll_head_node = current_array_value.get_head_node()
 
-        if current_ll_head_node == None:
+        if current_array_value == None:
             pass
         
-        if current_ll_head_node.get_value()[0] == key:
-            self.array[array_index] = DoubleLinkedList()
+        elif current_array_value[0] == key:
+            self.array[array_index] = None 
             return 
-        
         del_collisions = 1
-        while current_ll_head_node.get_value()[0] != key:
+        while current_array_value[0] != key:
             new_hash_code = self.hash(key, del_collisions)
             new_array_index = self.compressor(new_hash_code)
             new_array_value = self.array[new_array_index]
-            new_ll_head_node = new_array_value.get_head_node()
 
-            if new_ll_head_node == None:
-                pass 
-            
-            if new_ll_head_node.get_value()[0] == key:
-                self.array[new_array_index] = DoubleLinkedList()
+            if new_array_value == None:
+                pass
+            elif new_array_value[0] == key:
+                self.array[new_array_index] = None 
                 return 
             
             del_collisions += 1
-            return
+            return 
+
 
     def get_array(self):
         return self.array
