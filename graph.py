d6d88493e9ded66ec5542e5a57d71b474b2fcf2a
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-06-04 21:48:35 -0400

    Updates and Bug fixes

--- graph.py
+++ graph.py
@@ -6,30 +6,34 @@ class Graph:
         self.graph_dict = {}
 
     def add_vertex(self, vertex):
-        print('Adding vertex {}'.format(vertex.value))
-        self.graph_dict[vertex.value] = vertex 
+        print('Adding vertex {}'.format(vertex.value[0]))
+        self.graph_dict[vertex.value[0]] = vertex 
 
     def add_edge(self, from_vertex, to_vertex, weight=0):
-        self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
-        
+        self.graph_dict[from_vertex.value[0]].add_edge(to_vertex.value[0], weight)
         if not self.directed:
-            self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+            self.graph_dict[to_vertex.value[0]].add_edge(from_vertex.value[0], weight)
         
     def find_path(self, start_vertex, end_vertex):
         start = [start_vertex]
         seen = {}
         while len(start) > 0:
             current_vertex = start.pop()
-            print(current_vertex.value)
+            print(current_vertex)
             seen[current_vertex] = True 
 
             if current_vertex == end_vertex:
-                return True 
+                print("Link found! ")
+                return 
             else:
                 vertex = self.graph_dict[current_vertex]
                 next_vertices = vertex.get_edges()
                 next_vertices = [vertex for vertex in next_vertices if not vertex in seen]
                 start.extend(next_vertices)
         
-        return False 
+        print("There is no link between the two modems")
+        return  
+
+
+
 
--- script.py
+++ script.py
@@ -3,13 +3,17 @@ from graph import Graph
 from vertex import Vertex
 import hashlib
 import time 
+import random 
 
 
 
 class Prompt:
     wifibase = Graph()
-    unregisted_modems = [] 
+    unregistered_modems = [] 
+    registered_modems = []
+
     def database(self):
+        
         playing = True 
         while playing:
             print('\n')
@@ -18,16 +22,18 @@ class Prompt:
             if prompt == '/add_modem':
                 name = input('Name of the modem: ')
                 ip_address = input('First three numbers of ip: ')
-                ip_address = hash_lib.sha224(ip_address).hexdigest()
+                ip_address = ip_address.encode()
+                ip_address = hashlib.sha224(ip_address).hexdigest()
 
                 validation = False 
                 
-                for i in list(self.wifibase.graph_dict.keys()):
-                    if i[0] == name:
+                for i in self.unregistered_modems:
+                    if len(self.unregistered_modems) == 0:
+                        break 
+                    elif i.value[-1] == ip_address:
                         validation = True 
-                        break
-                    elif i[-1] == ip_address:
-                        validation = True 
+                    elif i.value[0] == name:
+                        validation = True
                         break
                     else:
                         continue 
@@ -39,15 +45,15 @@ class Prompt:
                 else:
                     modem_list = [name, ip_address]
                     vertex_modem = Vertex(modem_list)
-                    self.unregisted_modems.append(vertex_modem)
+                    self.unregistered_modems.append(vertex_modem)
                     time.sleep(0.1)
-                    print('New Modem added. Type \`/register\` to register the modem in to the system')
+                    print('New Modem added. Type \'/register\' to register the modem in to the system')
             
             elif prompt == "/register":
                 modem_name = input('Name of the modem: ')
                 validation = False 
                 modem = None
-                for i in self.unregisted_modems:
+                for i in self.unregistered_modems:
                     if i.value[0] == modem_name:
                         validation = True 
                         modem = i
@@ -63,53 +69,79 @@ class Prompt:
                         print('This is the first modem in the server, hence, there will be no links')
                         time.sleep(0.01)
                         self.wifibase.add_vertex(modem)
+                        self.registered_modems.append(modem)
                         print('Modem added')
                         time.sleep(0.1)
                         print('Once there are more modems (>1) defined in the software, you will be able to add links')
                     else:
                         for wifi in list(self.wifibase.graph_dict.keys()):
                             print('-'*24)
-                            print(wifi[0])
+                            print(wifi)
                         
-                        temp_modem_list = list(self.wifibase.graph_dict.keys())
-                        temp_modem_names = [i[0] for i in temp_modem_list]
+                        temp_modem_names = list(self.wifibase.graph_dict.keys())
                         print('-'*24)
                         print()
                         
                         print("Here are the modem(s) that are in the server ")
                         time.sleep(0.1)
-                        router_name = input('Please enter one of the modem\'s name exactly to add a link. If you wish to have no link, please type \'None\' ')
+                        router_name = input('Please enter one of the modem\'s name exactly to add a link. If you wish to have no link, please type \'None\': ')
 
                         if not router_name in temp_modem_names:                                     # If the link is not even defined 
                             print('That router does not seem to be in the sever ')
                             time.sleep(0.1)
                         elif router_name == "None" or router_name == "None ":                       # If the user wants no link
                             self.wifibase.add_vertex(modem)
+                            self.registered_modems.append(modem)
                             print('Modem added with no link ')
                             time.sleep(0.1)
                         
                         elif router_name in temp_modem_names:                                       # If link is defined
                             link_router = None 
                             for data, vertex in self.wifibase.graph_dict.items():
-                                if data[0] == router_name:
+                                if data == router_name:
                                     link_router = vertex 
                                     break 
                                 else:
                                     continue 
                             
-                            latency = random.randint(1, 1000)
+                            miliseconds = random.randint(1, 2500)
                             self.wifibase.add_vertex(modem)
-                            self.wifibase.add_edge(modem, link_router, latency)
+                            self.registered_modems.append(modem)
+                            self.wifibase.add_edge(modem, link_router, miliseconds)
 
-                            print('{modem1} is linked to {modem2} with {latency} ms '.format(modem1=modem, modem2=link_router, latency=latency))
+                            print('\'{modem1}\' is linked to \'{modem2}\' with a return speed of {latency} m/s '.format(modem1=modem.value[0], modem2=link_router.value[0], latency=miliseconds))
 
+
+            elif prompt == "/check_link":
+                for i in self.registered_modems:
+                    print("-"*24)
+                    print(i.value[0])
+                    time.sleep(0.1)
+                print("-"*24)
+                print("Here are the current registered modems on the server")
+                modem_one = input("Plese enter the first modem name: ")
+                modem_two = input('Please enter the second modem name: ' )
+
+                time.sleep(0.1)
+                #modem_one = self.wifibase.graph_dict[modem_one]
+                #modem_two = self.wifibase.graph_dict[modem_two]
+
+                print('Finding if there is a link between the two modems... ')
+                
+                for i in range(3):
+                    print("...")
+                    time.sleep(0.2)
+                
+                self.wifibase.find_path(modem_one, modem_two)
 
                             
 
 
 
 
+test = Prompt()
 
+test.database()
             
 
                 
