b7f28e56afca62e83590864c8d9404929e504cee
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-06-10 17:27:34 -0400

    Add files via upload

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import time
+from graph import Graph
 
 class Node:
     def __init__(self, value, link=None, prev_link=None):
@@ -26,6 +27,7 @@ class DoubleLinkedList:
     def __init__(self):
         self.head_node = None 
         self.tail_node = None 
+        nodes = []
     
     def add_to_head(self, new_value):
         new_head = Node(new_value)
@@ -36,6 +38,7 @@ class DoubleLinkedList:
             new_head.set_link(current_head)
         
         self.head_node = new_head 
+        nodes.append(new_value[0])
 
         if self.tail_node == None:
             self.tail_node = new_head 
@@ -50,9 +53,11 @@ class DoubleLinkedList:
             new_tail.set_prev_link(current_tail)
         
         self.tail_node = new_tail 
+        nodes.append(new_value[0])
 
         if self.head_node == None:
             self.head_node = new_tail 
+        
 
     
     def remove_head(self):
@@ -135,8 +140,62 @@ class DoubleLinkedList:
 
 
 
-database = DoubleLinkedList()
+server_base = DoubleLinkedList()
 
 
-def initial_prompt(database=database):
-    pass
+def initial_prompt(server_base=server_base):
+    print('Welcome to Connection! ')
+    print("You have 5 options: ")
+    time.sleep(0.1)
+    print("-"*24)
+    print('/add_server: To add a new server to track your modems/routers')
+    time.sleep(0.2)
+    print('-'*24) 
+    print('/remove_server: To remove a server/network from the software')
+    time.sleep(0.2)
+    print("-"*24)
+    print("/view_servers: To view all the servers registered in the software ")
+    time.sleep(0.2)
+    print('-'*24)
+    print("/access_server: To analyze and access one of the servers registered on the server" )
+    time.sleep(0.2)
+    print('-'*24)
+    print("/quit: To quit the program")
+    
+
+    start = True 
+    while start:
+        user_prompt = input(':')
+        
+        if user_prompt == '/add_server':
+            server_name = input('Please type in the name of the server ')
+            
+            if not server_name in server_base.nodes:
+                server_extra_info = input('Is there any other extra info you would like to add (*Type \'None\' if not applicable): ')
+                
+                if server_extra_info == None:
+                    new_server = [server_name, Graph()]
+                else:
+                    new_server = [server_name, Graph(), extra_info]
+
+            server_base.add_to_tail(new_server)
+            time.sleep(0.2)
+            print("Your Server has been added. To view it, please type \'/view_servers\' or to access it, type \'/access_server\'")
+        
+        elif user_prompt == '/remove_server':
+            sever_name = input('Please type in the name of the server: ')
+            if not server_name in server_base.nodes:
+                print("That server does not seem to be registered in this software currently ")
+                time.sleep(0.1)
+                print("Type \'/add_server\' to do so ")
+                time.sleep(0.1)
+            else:
+                server_base.remove_by_value(server_name)
+                time.sleep(0.1)
+                print('Server has been successfully deleted ')
+        
+        elif user_prompt == '/view_servers':
+            if len(server_base.nodes) == 0:
+                print("There currently is no server registered on the software yet. ")
+
+
